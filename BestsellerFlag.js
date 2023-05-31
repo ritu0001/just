088e6975864c1d7ca6ccc5eedc16bdb90e2edfbe
ritu0001 <91780950+ritu0001@users.noreply.github.com>
2023-05-31 17:16:18 +0530

    Update BestsellerFlag.js

--- BestsellerFlag.js
+++ BestsellerFlag.js
@@ -1,1 +1,13 @@
+import React from "react";
+
+const BestsellerFlag = (props) => {
+  if (props.isBestseller) {
+    return <span className="hotel__best-seller-flag">Bestseller</span>;
+  } else {
+    return null;
+  }
+};
+
+export default BestsellerFlag;
+
 
